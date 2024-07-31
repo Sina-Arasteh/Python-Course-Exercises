37ad0e75070133f3a0b022a655fea2a0c19a1c55
Sina Arasteh <sinna.arasteh@gmail.com>
2024-07-31 18:17:48 +0330

    Add testings

--- 001 exercise.py
+++ 001 exercise.py
@@ -2,3 +2,6 @@ def reverse_string(a):
     if type(a) != str:
         raise TypeError("Please insert a string.")
     return a[::-1]
+
+print(reverse_string("Hello World!!!"))
+print(reverse_string(54))
