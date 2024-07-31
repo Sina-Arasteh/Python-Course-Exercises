4cd9c13c023fa17a9c317dc3383ef2727a54c289
Sina Arasteh <sinna.arasteh@gmail.com>
2024-07-31 18:58:41 +0330

    Some changes

--- 001 exercise.py
+++ 001 exercise.py
@@ -4,16 +4,17 @@ def reverse_string(a):
     return a[::-1]
 
 def list_check(a):
-    for value in a:
-        if type(value) != list:
-            return False
-        return True
+    return all([type(value) == list for value in a])
+#    for value in a:
+#        if type(value) != list:
+#            return False
+#        return True
 
 
 
 #print(reverse_string("Hello World!!!"))
 #print(reverse_string(54))
 
-print(list_check([[],[1],[2,3], (1,2)]))
-print(list_check([1, True, [],[1],[2,3]]))
-print(list_check([[],[1],[2,3]]))
+print(list_check([[], [1], [2, 3], (1, 2)]))
+print(list_check([1, True, [], [1], [2, 3]]))
+print(list_check([[], [1], [2, 3]]))
