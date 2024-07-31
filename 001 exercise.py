1bd8fb3ad7e9f5e7d915e021992b7c2c3385bef8
Sina Arasteh <sinna.arasteh@gmail.com>
2024-07-31 18:28:58 +0330

    Defining the list_check function

--- 001 exercise.py
+++ 001 exercise.py
@@ -3,5 +3,11 @@ def reverse_string(a):
         raise TypeError("Please insert a string.")
     return a[::-1]
 
+def list_check(a):
+    for value in a:
+        if type(value) != list:
+            return False
+        return True
+
 print(reverse_string("Hello World!!!"))
 print(reverse_string(54))
