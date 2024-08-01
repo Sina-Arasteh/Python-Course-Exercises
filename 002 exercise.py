8988015202d654268925165c8a485fa431ef3e16
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-02 00:12:24 +0330

    Add the testing codes of repeat function

--- 002 exercise.py
+++ 002 exercise.py
@@ -17,21 +17,26 @@ def repeat(a, b):
 
 
 
-#The testing codes of titleize:
+#The testing codes of titleize function:
 #print(titleize('this is awesome'))
 #print(titleize('oNLy cAPITALIZe fIRSt'))
 
-#The testing codes of find_factors: 
+#The testing codes of find_factors function: 
 #print(find_factors(10))
 #print(find_factors(11))
 #print(find_factors(111))
 #print(find_factors(321421))
 #print(find_factors(412146))
 
-#The testing codes of includes:
+#The testing codes of includes function:
 #print(includes([1, 2, 3], 1))
 #print(includes([1, 2, 3], 1, 2))
 #print(includes({ 'a': 1, 'b': 2 }, 1)) 
 #print(includes({ 'a': 1, 'b': 2 }, 'a'))
 #print(includes('abcd', 'b'))
 #print(includes('abcd', 'e'))
+
+#The testing codes of repeat function:
+print(repeat('*', 3))
+print(repeat('abc', 2))
+print(repeat('abc', 0))
