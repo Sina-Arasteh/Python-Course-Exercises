5317eaf4787caad3fcc8ec279e6bebd27f129d90
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-01 22:40:38 +0330

    Comment out the testing codes fo find_factors function + some changes

--- 002 exercise.py
+++ 002 exercise.py
@@ -6,12 +6,13 @@ def find_factors(a):
     return list(filter(lambda i: a % i == 0, range(1, a+1)))
 
 
-
+#titleize's testing codes:
 #print(titleize('this is awesome'))
 #print(titleize('oNLy cAPITALIZe fIRSt'))
 
-print(find_factors(10))
-print(find_factors(11))
-print(find_factors(111))
-print(find_factors(321421))
-print(find_factors(412146))
+#find_factors' testing codes: 
+#print(find_factors(10))
+#print(find_factors(11))
+#print(find_factors(111))
+#print(find_factors(321421))
+#print(find_factors(412146))
