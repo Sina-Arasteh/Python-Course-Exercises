077c6ab3116f83bcfcff7e113a49e06098b4e652
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-01 22:28:33 +0330

    Add the testing codes of find_factors function

--- 002 exercise.py
+++ 002 exercise.py
@@ -5,5 +5,13 @@ def titleize(a):
 def find_factors(a):
     return lambda i: i <= a; a % i == 0
 
+
+
 #print(titleize('this is awesome'))
 #print(titleize('oNLy cAPITALIZe fIRSt'))
+
+print(find_factors(10))
+print(find_factors(11))
+print(find_factors(111))
+print(find_factors(321421))
+print(find_factors(412146))
