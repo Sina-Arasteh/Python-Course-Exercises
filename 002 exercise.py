45ea0813bb24f5b7bd34c40b2eddfd9d1d5c9c46
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-01 22:26:42 +0330

    Define the first version of find_factors function

--- 002 exercise.py
+++ 002 exercise.py
@@ -2,7 +2,8 @@ def titleize(a):
     c = [i.capitalize() for i in a.split(" ")]
     return " ".join(c)
 
-
+def find_factors(a):
+    return lambda i: i <= a; a % i == 0
 
 #print(titleize('this is awesome'))
 #print(titleize('oNLy cAPITALIZe fIRSt'))
