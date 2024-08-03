24fe4b7dc398d12d5d545c69c43bfe7c822900a2
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-03 23:30:49 +0330

    Comment out the testing codes of the 'two_list_dictionary' function

--- My answers/003 answers.py
+++ My answers/003 answers.py
@@ -12,6 +12,6 @@ def two_list_dictionary(list1, list2):
 
 
 #The test codings of the "two_list_dictionary" function:
-print(two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3])) # {'a': 1, 'b': 2, 'c': 3, 'd': None}
-print(two_list_dictionary(['a', 'b', 'c']  , [1, 2, 3, 4])) # {'a': 1, 'b': 2, 'c': 3}
-print(two_list_dictionary(['x', 'y', 'z']  , [1,2])) # {'x': 1, 'y': 2, 'z': None}
+#print(two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3])) # {'a': 1, 'b': 2, 'c': 3, 'd': None}
+#print(two_list_dictionary(['a', 'b', 'c']  , [1, 2, 3, 4])) # {'a': 1, 'b': 2, 'c': 3}
+#print(two_list_dictionary(['x', 'y', 'z']  , [1,2])) # {'x': 1, 'y': 2, 'z': None}
