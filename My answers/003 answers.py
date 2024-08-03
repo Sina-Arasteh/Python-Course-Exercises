2439d646a66745ff21e2c967f4e52feff3fdc523
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-03 23:26:42 +0330

    Add the testing codes of the 'two_list_dictionary' function

--- My answers/003 answers.py
+++ My answers/003 answers.py
@@ -9,3 +9,9 @@ def two_list_dictionary(list1, list2):
     else:
         return {list1[i]: list2[i] for i in range(len(list1))}
 
+
+
+#The test codings of the "two_list_dictionary" function:
+print(two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3])) # {'a': 1, 'b': 2, 'c': 3, 'd': None}
+print(two_list_dictionary(['a', 'b', 'c']  , [1, 2, 3, 4])) # {'a': 1, 'b': 2, 'c': 3}
+print(two_list_dictionary(['x', 'y', 'z']  , [1,2])) # {'x': 1, 'y': 2, 'z': None}
