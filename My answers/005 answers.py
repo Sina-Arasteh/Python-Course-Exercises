ab9a1dee149fe0dcc2d05a08ffa9812d02b6a670
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-05 23:31:11 +0330

    Define the 'reverse_vowels' function

--- My answers/005 answers.py
+++ My answers/005 answers.py
@@ -9,6 +9,15 @@ def valid_parentheses(paren_string):
             return False
     return count == 0
 
+def reverse_vowels(string):
+    vowels = "aAeEiIoOuU"
+    list_index = []
+    for char in string:
+        if char in vowels:
+            list_index.append(string.index(char))
+    for i in range(1, len()):
+        string.replace(string[list_index[i]], string[list_index[-i-1]])
+    return string
 
 
 #The testing codes of the "valid_parentheses" function:
