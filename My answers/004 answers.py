5d0ba3700170bdf4803e4b5f5ba60d16804c6cb4
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-05 19:08:49 +0330

    Comment out the testing codes of the 'find_greater-numbers' function

--- My answers/004 answers.py
+++ My answers/004 answers.py
@@ -52,7 +52,7 @@ def find_greater_numbers(list):
 #print(min_max_key_in_dictionary({1: "Elie", 4:"Matt", 2: "Tim"})) # [1,4]
 
 #The testing codes of the "find_greater_numbers" function:
-print(find_greater_numbers([1,2,3])) # 3 
-print(find_greater_numbers([6,1,2,7])) # 4
-print(find_greater_numbers([5,4,3,2,1])) # 0
-print(find_greater_numbers([])) # 0
+#print(find_greater_numbers([1,2,3])) # 3 
+#print(find_greater_numbers([6,1,2,7])) # 4
+#print(find_greater_numbers([5,4,3,2,1])) # 0
+#print(find_greater_numbers([])) # 0
