288565f8e73407e908435ac1ba94bd527172a7f1
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-05 22:47:15 +0330

    Modify the 'valid_parentheses' function

--- My answers/005 answers.py
+++ My answers/005 answers.py
@@ -1,13 +1,13 @@
 def valid_parentheses(paren_string):
-    if len(paren_string) % 2 != 0:
-        return False
-    else:
-        for char in paren_string:
-            if char == "(" and paren_string.index(char) % 2 != 0:
-                return False
-            if char == ")" and paren_string.index(char) % 2 == 0:
-                return False
-        return True
+    count = 0
+    for char in paren_string:
+        if char == "(":
+            count += 1
+        elif char == ")":
+            count -= 1
+        if count < 0:
+            return False
+    return count == 0
 
 
 
