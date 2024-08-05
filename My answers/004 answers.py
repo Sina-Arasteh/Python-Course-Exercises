971de2d98923429da417eef692aea23acadce770
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-05 18:40:49 +0330

    Comment out the testing codes of the 'sum_up_diagonals' function

--- My answers/004 answers.py
+++ My answers/004 answers.py
@@ -6,34 +6,30 @@ def sum_up_diagonals(list):
 
 
 #The testing codes of the "sum_up_diagonals" function:
-list1 = [
-  [ 1, 2 ],
-  [ 3, 4 ]
-]
- 
-sum_up_diagonals(list1) # 10
-
-list2 = [
-  [ 1, 2, 3 ],
-  [ 4, 5, 6 ],
-  [ 7, 8, 9 ]
-]
- 
-sum_up_diagonals(list2) # 30
- 
-list3 = [
-  [ 4, 1, 0 ],
-  [ -1, -1, 0],
-  [ 0, 0, 9]
-]
-
-sum_up_diagonals(list3) # 11
-
-list4 = [
-  [ 1, 2, 3, 4 ],
-  [ 5, 6, 7, 8 ],
-  [ 9, 10, 11, 12 ],
-  [ 13, 14, 15, 16 ]
-]
- 
-sum_up_diagonals(list4) # 68
+#list1 = [
+#  [ 1, 2 ],
+#  [ 3, 4 ]
+#]
+#print(sum_up_diagonals(list1)) # 10
+#
+#list2 = [
+#  [ 1, 2, 3 ],
+#  [ 4, 5, 6 ],
+#  [ 7, 8, 9 ]
+#]
+#print(sum_up_diagonals(list2)) # 30
+# 
+#list3 = [
+#  [ 4, 1, 0 ],
+#  [ -1, -1, 0],
+#  [ 0, 0, 9]#
+#]
+#print(sum_up_diagonals(list3)) # 11
+#
+#list4 = [
+#  [ 1, 2, 3, 4 ],
+#  [ 5, 6, 7, 8 ],
+#  [ 9, 10, 11, 12 ],
+#  [ 13, 14, 15, 16 ]
+#]
+#print(sum_up_diagonals(list4)) # 68
