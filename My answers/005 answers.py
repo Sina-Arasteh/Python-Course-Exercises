03bea28e7379db6a0a0541e4f47e22f12e291764
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-05 22:10:04 +0330

    Define the 'valid_parentheses' function

---
+++ My answers/005 answers.py
@@ -0,0 +1,13 @@
+def valid_parentheses(paren_string):
+    if len(paren_string) % 2 != 0:
+        return False
+    else:
+        for char in paren_string:
+            if char == "(" and paren_string.index(char) % 2 != 0:
+                return False
+            if char == ")" and paren_string.index(char) % 2 == 0:
+                return False
+        return True
+
+
+
