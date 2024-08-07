470512a8e8af94da113dcc8c7be865cc7913f6ce
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-07 13:25:09 +0330

    Add the testing codes of the 'letter_counter' function

--- My answers/007 answers.py
+++ My answers/007 answers.py
@@ -3,3 +3,12 @@ def letter_counter(string):
         return string.count(letter)
     return count
 
+
+
+#The testing codes of the "letter_counter" function:
+counter = letter_counter('Amazing')
+counter('a') # 2
+counter('m') # 1
+counter2 = letter_counter('This Is Really Fun!')
+counter2('i') # 2
+counter2('t') # 1
