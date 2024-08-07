b625cbc8c96c4df766812f4519510ea926ed1e68
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-07 15:13:16 +0330

    Comment out the testing codes of the 'next_prime' function

--- My answers/007 answers.py
+++ My answers/007 answers.py
@@ -47,5 +47,5 @@ def next_prime():
 #print(oneAddition(12,200)) # None
 
 #The testing codes of the "next_prime" function:
-primes = next_prime()
-print([next(primes) for i in range(25)]) # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+#primes = next_prime()
+#print([next(primes) for i in range(25)]) # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
