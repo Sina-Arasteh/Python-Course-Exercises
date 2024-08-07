22f6bd88b847fed6f5cd3109d823a7e162ba53a2
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-07 13:52:23 +0330

    Comment out the testing codes of the 'letter_counter' function

--- My answers/007 answers.py
+++ My answers/007 answers.py
@@ -1,14 +1,14 @@
 def letter_counter(string):
     def count(letter):
-        return string.count(letter)
+        return string.lower().count(letter)
     return count
 
 
 
 #The testing codes of the "letter_counter" function:
-counter = letter_counter('Amazing')
-counter('a') # 2
-counter('m') # 1
-counter2 = letter_counter('This Is Really Fun!')
-counter2('i') # 2
-counter2('t') # 1
+#counter = letter_counter('Amazing')
+#print(counter('a')) # 2
+#print(counter('m')) # 1
+#counter2 = letter_counter('This Is Really Fun!')
+#print(counter2('i')) # 2
+#print(counter2('t')) # 1
