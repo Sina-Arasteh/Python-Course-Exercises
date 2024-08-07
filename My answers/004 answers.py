af4e0a60b3f20bbf3f9de501e8cd6e35922de4c7
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-07 16:55:02 +0330

    Improve the '004  answers.py' file

--- My answers/004 answers.py
+++ My answers/004 answers.py
@@ -34,47 +34,47 @@ def is_odd_string(string):
 
 #The testing codes of the "sum_up_diagonals" function:
 #list1 = [
-#  [ 1, 2 ],
-#  [ 3, 4 ]
+#  [1, 2],
+#  [3, 4]
 #]
 #print(sum_up_diagonals(list1)) # 10
 #
 #list2 = [
-#  [ 1, 2, 3 ],
-#  [ 4, 5, 6 ],
-#  [ 7, 8, 9 ]
+#  [1, 2, 3],
+#  [4, 5, 6],
+#  [7, 8, 9]
 #]
 #print(sum_up_diagonals(list2)) # 30
 # 
 #list3 = [
-#  [ 4, 1, 0 ],
-#  [ -1, -1, 0],
-#  [ 0, 0, 9]#
+#  [4, 1, 0],
+#  [-1, -1, 0],
+#  [0, 0, 9]
 #]
 #print(sum_up_diagonals(list3)) # 11
 #
 #list4 = [
-#  [ 1, 2, 3, 4 ],
-#  [ 5, 6, 7, 8 ],
-#  [ 9, 10, 11, 12 ],
-#  [ 13, 14, 15, 16 ]
+#  [1, 2, 3, 4],
+#  [5, 6, 7, 8],
+#  [9, 10, 11, 12],
+#  [13, 14, 15, 16]
 #]
 #print(sum_up_diagonals(list4)) # 68
 
 #The testing codes of the "min_max_key_in_dictionary" function:
-#print(min_max_key_in_dictionary({2:'a', 7:'b', 1:'c',10:'d',4:'e'})) # [1,10]
-#print(min_max_key_in_dictionary({1: "Elie", 4:"Matt", 2: "Tim"})) # [1,4]
+#print(min_max_key_in_dictionary({2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e'})) # [1, 10]
+#print(min_max_key_in_dictionary({1: "Elie", 4: "Matt", 2: "Tim"})) # [1, 4]
 
 #The testing codes of the "find_greater_numbers" function:
-#print(find_greater_numbers([1,2,3])) # 3 
-#print(find_greater_numbers([6,1,2,7])) # 4
-#print(find_greater_numbers([5,4,3,2,1])) # 0
+#print(find_greater_numbers([1, 2, 3])) # 3
+#print(find_greater_numbers([6, 1, 2, 7])) # 4
+#print(find_greater_numbers([5, 4, 3, 2, 1])) # 0
 #print(find_greater_numbers([])) # 0
 
 #The testing codes of the "two_oldest_ages" function:
-#print(two_oldest_ages( [1, 2, 10, 8] )) # [8, 10]
-#print(two_oldest_ages( [6,1,9,10,4] )) # [9,10]
-#print(two_oldest_ages( [4,25,3,20,19,5] )) # [20,25]
+#print(two_oldest_ages([1, 2, 10, 8])) # [8, 10]
+#print(two_oldest_ages([6, 1, 9, 10, 4])) # [9, 10]
+#print(two_oldest_ages([4, 25, 3, 20, 19, 5])) # [20, 25]
 
 #The testing codes of the "is_odd_string" function:
 #print(is_odd_string('a')) # True
